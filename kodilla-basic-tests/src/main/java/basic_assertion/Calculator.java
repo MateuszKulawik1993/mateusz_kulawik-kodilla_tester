9c8e7797fcd294554f8182cf9238fbf23f3c2b67
Mateusz Kulawik <mateusz.kulawik.1993@gmail.com>
2024-09-24 10:26:58 +0200

    Initial version

---
+++ kodilla-basic-tests/src/main/java/basic_assertion/Calculator.java
@@ -0,0 +1,16 @@
+package basic_assertion;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double Math.pow(double c,2){
+        return c^2;
+    }
+}
