9fdd65e285d8a199d0dd3022283d4ba2f498dbee
Mateusz Kulawik <mateusz.kulawik.1993@gmail.com>
2024-09-25 11:08:52 +0200

    Initial version

--- kodilla-basic-tests/src/main/java/inheritance/homework/Doctor.java
+++ kodilla-basic-tests/src/main/java/inheritance/homework/Doctor.java
@@ -1,19 +1,21 @@
 package inheritance.homework;
 
-public class Doctor extends Job{
+public class Doctor extends Job {
 
     public Doctor() {
         super(9000, "Treat people");
     }
 
     @Override
-    public void earnMoney(){
-        System.out.println("My doctor earns"+ ""+""+salary);
+    public void earnMoney() {
+        System.out.println("My doctor earns" + "" + "" + salary);
 
     }
+
     @Override
-    public void mainResponsibility(){
-        System.out.println( "Doctors" +""+ responsibilities+""+""+"poeple.");
+    public void mainResponsibility() {
+        System.out.println("Doctors" + "" + responsibilities + "" + "" + "poeple.");
 
     }
 }
+
---
+++ kodilla-basic-tests/src/test/java/basic_assertion/Test.java
@@ -0,0 +1,4 @@
+package basic_assertion;
+
+public @interface Test {
+}
