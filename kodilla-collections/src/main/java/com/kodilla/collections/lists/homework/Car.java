9245235ce387924a6fd0c64213524e69d5e9e992
Mateusz Kulawik <mateusz.kulawik.1993@gmail.com>
2024-10-01 10:37:20 +0200

    Initial commit

--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/Car.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/Car.java
@@ -1,4 +1,11 @@
 package com.kodilla.collections.lists.homework;
 
 public interface Car {
+
+    int getSpeed();
+    void increaseSpeed();
+    void decreaseSpeed();
+
+
+
 }
--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarUtils.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarUtils.java
@@ -1,4 +1,10 @@
 package com.kodilla.collections.lists.homework;
 
 public class CarUtils {
+    public static void describeCar(Car car) {
+        System.out.println("Car details:");
+        System.out.println("Speed: " + car.getSpeed() + " km/h");
+    }
+
+
 }
