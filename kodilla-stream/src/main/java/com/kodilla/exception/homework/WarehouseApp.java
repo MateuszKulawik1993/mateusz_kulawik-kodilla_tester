ace4e0a8efc39af7c653434e764764ade72adef1
Mateusz Kulawik <mateusz.kulawik1993@gmail.com>
2024-10-17 09:59:23 +0200

    Initial version

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -1,4 +1,25 @@
 package com.kodilla.exception.homework;
 
 public class WarehouseApp {
+    public static void main(String[] args) {
+        Warehouse warehouse = new Warehouse();
+
+        warehouse.addOrder(new Order("123"));
+        warehouse.addOrder(new Order("456"));
+        warehouse.addOrder(new Order("789"));
+
+        try {
+            Order order = warehouse.getOrder("456");
+            System.out.println("Found order with number: " + order.getNumber());
+        } catch (OrderDoesntExistException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Order order = warehouse.getOrder("999");
+            System.out.println("Found order with number: " + order.getNumber());
+        } catch (OrderDoesntExistException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- kodilla-stream/src/test/java/stream/WarehouseAppTest.java
+++ kodilla-stream/src/test/java/stream/WarehouseAppTest.java
@@ -1,4 +1,23 @@
 package stream;
 
+import com.kodilla.exception.homework.Order;
+import com.kodilla.exception.homework.OrderDoesntExistException;
+import com.kodilla.exception.homework.Warehouse;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class WarehouseAppTest {
+    @Test
+    void testGetOrderThrowsException() {
+        // Given
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("123"));
+        warehouse.addOrder(new Order("456"));
+        warehouse.addOrder(new Order("789"));
+
+        // When & Then
+        assertThrows(OrderDoesntExistException.class, () -> warehouse.getOrder("999"));
+    }
+}
 }
